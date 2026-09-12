40bc6c76c9027abac4b11c69c3027bcb7d66d0e4
shve3678 <146483163+shve3678@users.noreply.github.com>
2026-09-11 19:44:29 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-  ]
-});
+  plugins: [react()],
+  base: '/', // or your repo-specific path, as discussed
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+})
