c63829d907796d91e48c9bb536df14e9fede1951
shve3678 <146483163+shve3678@users.noreply.github.com>
2026-09-14 19:56:44 -0700

    Update Creative.jsx

--- src/pages/Creative.jsx
+++ src/pages/Creative.jsx
@@ -12,7 +12,7 @@ const hobbies = [
     img: bharatanatyamImg,
     fit: "contain",
     aspect: "aspect-[3/4]",
-    imgMaxWidth: "max-w-sm",
+    imgMaxWidth: "max-w-xs",
     update: "Check out my latest work: a dance film exploring what it means to be a classical art form",
     linkText: "dance film",
     linkUrl: "https://www.youtube.com/watch?v=Gblr5kVXm24&list=RDGblr5kVXm24&start_radio=1",
